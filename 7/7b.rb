8573ac93bc48eb9f64e342b231ee02695362651b
andreamazzatxt <andreamazza89@gmail.com>
2021-10-10 23:20:18 +0200

    add day 7 part 2

--- 7/7b.rb
+++ 7/7b.rb
@@ -1,120 +1,115 @@
 input = File.read('7.txt').split(',').map { |int| int.to_i}
 class IntCode
-    def initialize(inputs, last = false)
+    def initialize(inputs, options)
         @instructions = inputs
-        @state = :processing
-        @pointer = 0
+        @options = options
+        @status = :continue
+        @current = 0
         @result = 0
-        @nextAmp = nil
-        @last = last
     end
 
-    def decode!(input = nil)
-        @input = input unless input.nil? 
-        instruction = parse_instruction @instructions[@pointer]
-        case instruction[:opcode]
-        when 1
-            handle_one instruction[:mode]
-        when 2
-            handle_two instruction[:mode]
-        when 3
-            handle_three instruction[:mode]
-        when 4 
-            handle_four instruction[:mode]
-        when 5
-            handle_five instruction[:mode]
-        when 6 
-            handle_six instruction[:mode]
-        when 7
-            handle_seven instruction[:mode]
-        when 8 
-            handle_eight instruction[:mode] 
-        else
-            raise 'Error OPCODE'
+    def decode!(value = nil)
+        @status = :continue
+        @options << value if !value.nil?
+        while @status == :continue && @instructions[@current] != 99
+            instruction = parse_instruction @instructions[@current]
+            case instruction[:opcode]
+            when 1
+                handle_one instruction[:mode]
+            when 2
+                handle_two instruction[:mode]
+            when 3
+                handle_three instruction[:mode]
+            when 4 
+                handle_four instruction[:mode]
+            when 5
+                handle_five instruction[:mode]
+            when 6 
+                handle_six instruction[:mode]
+            when 7
+                handle_seven instruction[:mode]
+            when 8 
+                handle_eight instruction[:mode]
+            else
+               puts(' ERROR : OP not Valid ' + @instructions[@current].to_s) 
+            break
+            end
         end
-
-        case(@state)
-        when :halt
-            return @next_amp.decode!(@result) unless @last
-            return @result
-        when :processing
-            decode!()
-        end
+        @status = :halt if @instructions[@current] == 99
+        return @result
     end
 
-    def send_to(send_to)
-        @next_amp = send_to
-    end
-
-    def set_phase(phase)
-        @phase = phase
+    def halted?
+        @status == :halt
     end
 
     private
 
     def handle_one(modes)
-        input1 = modes[:one].zero? ? @instructions[@instructions[@pointer + 1]] : @instructions[@pointer + 1] 
-        input2 = modes[:two].zero? ? @instructions[@instructions[@pointer + 2]] : @instructions[@pointer + 2]
-        target = @instructions[@pointer + 3]
+        input1 = modes[:one].zero? ? @instructions[@instructions[@current + 1]] : @instructions[@current + 1] 
+        input2 = modes[:two].zero? ? @instructions[@instructions[@current + 2]] : @instructions[@current + 2]
+        target = @instructions[@current + 3]
         @instructions[target] = input1 + input2
-        @pointer = @pointer + 4
+        @current = @current + 4
     end
 
     def handle_two(modes)
-        input1 = modes[:one].zero? ? @instructions[@instructions[@pointer + 1]] : @instructions[@pointer + 1] 
-        input2 = modes[:two].zero? ? @instructions[@instructions[@pointer + 2]] : @instructions[@pointer + 2]
-        target = @instructions[@pointer + 3]
+        input1 = modes[:one].zero? ? @instructions[@instructions[@current + 1]] : @instructions[@current + 1] 
+        input2 = modes[:two].zero? ? @instructions[@instructions[@current + 2]] : @instructions[@current + 2]
+        target = @instructions[@current + 3]
         @instructions[target] = input1 * input2
-        @pointer = @pointer + 4
+        @current = @current + 4
     end
 
     def handle_three(modes)
-        target = @instructions[@pointer + 1]
-        @instructions[target] = @input
-        @pointer = @pointer + 2
+        input1 = @options.shift
+        target = @instructions[@current + 1]
+        @instructions[target] = input1
+        @current = @current + 2
     end
 
     def handle_four(modes)
-        @result = modes[:one].zero? ? @instructions[@instructions[@pointer + 1]] : @instructions[@pointer + 1]
-        @pointer = @pointer + 2
-        @state = :halt
+        @status= :pause
+        @result = modes[:one].zero? ? @instructions[@instructions[@current + 1]] : @instructions[@current + 1]
+        @current = @current + 2
     end
 
     def handle_five(modes)
-        input1 = modes[:one].zero? ? @instructions[@instructions[@pointer + 1]] : @instructions[@pointer + 1] 
-        input2 = modes[:two].zero? ? @instructions[@instructions[@pointer + 2]] : @instructions[@pointer + 2]
+        input1 = modes[:one].zero? ? @instructions[@instructions[@current + 1]] : @instructions[@current + 1] 
+        input2 = modes[:two].zero? ? @instructions[@instructions[@current + 2]] : @instructions[@current + 2]
         if !input1.zero?
-            @pointer = input2
+            @current = input2
         else
-            @pointer = @pointer + 3
+            @current = @current + 3
         end
     end
 
     def handle_six(modes)
-        input1 = modes[:one].zero? ? @instructions[@instructions[@pointer + 1]] : @instructions[@pointer + 1] 
-        input2 = modes[:two].zero? ? @instructions[@instructions[@pointer + 2]] : @instructions[@pointer + 2]
+        input1 = modes[:one].zero? ? @instructions[@instructions[@current + 1]] : @instructions[@current + 1] 
+        input2 = modes[:two].zero? ? @instructions[@instructions[@current + 2]] : @instructions[@current + 2]
         if input1.zero?
-            @pointer = input2
+            @current = input2
         else
-            @pointer = @pointer + 3
+            @current = @current + 3
         end
     end
 
     def handle_seven(modes)
-        input1 = modes[:one].zero? ? @instructions[@instructions[@pointer + 1]] : @instructions[@pointer + 1] 
-        input2 = modes[:two].zero? ? @instructions[@instructions[@pointer + 2]] : @instructions[@pointer + 2]
-        target = @instructions[@pointer + 3]
+        input1 = modes[:one].zero? ? @instructions[@instructions[@current + 1]] : @instructions[@current + 1] 
+        input2 = modes[:two].zero? ? @instructions[@instructions[@current + 2]] : @instructions[@current + 2]
+        target = @instructions[@current + 3]
         @instructions[target] = input1 < input2 ? 1 : 0
-        @pointer = @pointer + 4
+        @current = @current + 4
     end
 
     def handle_eight(modes)
-        input1 = modes[:one].zero? ? @instructions[@instructions[@pointer + 1]] : @instructions[@pointer + 1] 
-        input2 = modes[:two].zero? ? @instructions[@instructions[@pointer + 2]] : @instructions[@pointer + 2]
-        target = @instructions[@pointer + 3]
+        input1 = modes[:one].zero? ? @instructions[@instructions[@current + 1]] : @instructions[@current + 1] 
+        input2 = modes[:two].zero? ? @instructions[@instructions[@current + 2]] : @instructions[@current + 2]
+        target = @instructions[@current + 3]
         @instructions[target] = input1 == input2 ? 1 : 0
-        @pointer = @pointer + 4
+        @current = @current + 4
     end
+
 
     def parse_instruction(instruction)
         instruction = instruction.to_s.rjust(5, '0')
@@ -129,41 +124,35 @@ class IntCode
     end
 end
 
+phases_comb = [5, 6, 7 , 8, 9].permutation(5).to_a
+highest = 0
+
 class Circuit
     def initialize(input, phases)
-        @ampE = IntCode.new(input, true)
-        @ampD = IntCode.new(input)
-        @ampC = IntCode.new(input)
-        @ampB = IntCode.new(input)
-        @ampA = IntCode.new(input)
-        @ampA.send_to(@ampB)
-        @ampB.send_to(@ampC)
-        @ampC.send_to(@ampD)
-        @ampD.send_to(@ampE)
-        @ampE.send_to(@ampA)
-        set_phases(phases)
+        @phases = phases
+        @amp_a = IntCode.new(input, [phases[0]])
+        @amp_b = IntCode.new(input, [phases[1]])
+        @amp_c = IntCode.new(input, [phases[2]])
+        @amp_d = IntCode.new(input, [phases[3]])
+        @amp_e = IntCode.new(input, [phases[4]])
+        @circuit = [@amp_a, @amp_b, @amp_c, @amp_d, @amp_e]
     end
 
-    def start!
-        return @ampA.decode!(0)
-    end
-    private
-    def set_phases(array)
-        @ampA.set_phase(array[0])
-        @ampB.set_phase(array[1])
-        @ampC.set_phase(array[2])
-        @ampD.set_phase(array[3])
-        @ampE.set_phase(array[4])
+    def run!
+        current_output = 0
+        while !@amp_e.halted?
+            current_output = @circuit.reduce(current_output) do |value, amp|
+                amp.decode!(value)
+            end
+        end
+        return current_output
     end
 end
 
-phases_comb = [9,8,7,6,5].permutation(5).to_a
-highest = nil
-phases_comb.each do |comb|
-    circuit = Circuit.new(input,comb)
-    result = circuit.start!
-    highest = result if highest.nil? || result > highest
+phases_comb.each do |phases|
+   output = Circuit.new(input, phases).run!
+   highest = output if output > highest
 end
 
-
-puts "Part Two: #{highest}"
+puts 'Part Two: '
+p highest
